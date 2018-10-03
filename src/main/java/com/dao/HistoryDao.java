42309ed909141e9f0ce72b775f2789f602f1502a
Yodev <natlove.natlove202@gmail.com>
2018-10-03 18:16:57 +0700

    commit file History

---
+++ src/main/java/com/dao/HistoryDao.java
@@ -0,0 +1,25 @@
+package com.dao;
+
+public class HistoryDao {
+	int hisorygetpillowId;
+	public int getHisorygetpillowId() {
+		return hisorygetpillowId;
+	}
+	public void setHisorygetpillowId(int hisorygetpillowId) {
+		this.hisorygetpillowId = hisorygetpillowId;
+	}
+	public int getTimetogetpillowId() {
+		return timetogetpillowId;
+	}
+	public void setTimetogetpillowId(int timetogetpillowId) {
+		this.timetogetpillowId = timetogetpillowId;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	int timetogetpillowId;
+	int status;
+}
---
+++ src/main/java/com/dto/HistoryDto.java
@@ -0,0 +1,25 @@
+package com.dto;
+
+public class HistoryDto {
+	int hisorygetpillowId;
+	int timetogetpillowId;
+	int status;
+	public int getHisorygetpillowId() {
+		return hisorygetpillowId;
+	}
+	public void setHisorygetpillowId(int hisorygetpillowId) {
+		this.hisorygetpillowId = hisorygetpillowId;
+	}
+	public int getTimetogetpillowId() {
+		return timetogetpillowId;
+	}
+	public void setTimetogetpillowId(int timetogetpillowId) {
+		this.timetogetpillowId = timetogetpillowId;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+}
